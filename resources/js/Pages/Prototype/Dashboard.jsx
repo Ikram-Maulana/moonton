6dc6be4d38483daabd33e1a943864bcd5377f91d
Ikram Maulana <ikram075@ummi.ac.id>
2022-11-14 05:30:59 +0700

    Feat: Slicing Browse Movies

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -4,6 +4,7 @@ import Flickity from "react-flickity-component";
 
 // CSS for Flickity
 import "flickity/css/flickity.css";
+import { Head } from "@inertiajs/inertia-react";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -17,65 +18,115 @@ export default function Dashboard() {
     };
 
     return (
-        <Authenticated>
-            {/* Featured */}
-            <div>
-                <div className="font-semibold text-[22px] text-black mb-4">
-                    Featured Movies
-                </div>
+        <>
+            <Head>
+                <title>Dashboard</title>
+            </Head>
 
-                <Flickity className="gap-[30px]" options={flickityOptions}>
-                    {[...Array(4)].map((i) => (
-                        <div
-                            className="absolute overflow-hidden group mr-[30px]"
-                            key={i}
-                        >
-                            <img
-                                src="/images/featured-1.png"
-                                className="object-cover rounded-[30px] w-[520px] h-[340px]"
-                                alt=""
-                            />
+            <Authenticated>
+                {/* Featured */}
+                <div>
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Featured Movies
+                    </div>
 
-                            {/* rating */}
-                            <div className="rating absolute top-0 left-0">
-                                <div className="p-[30px] flex items-center gap-1">
-                                    <img src="/icons/ic_star.svg" alt="" />
-                                    <span className="text-sm font-medium text-white mt-1">
-                                        4.5/5.0
-                                    </span>
-                                </div>
-                            </div>
-
-                            {/* bottom detail */}
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[...Array(4)].map((i) => (
                             <div
-                                className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
-                          rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
+                                className="absolute overflow-hidden group mr-[30px]"
+                                key={i}
                             >
-                                <div>
-                                    <div className="font-medium text-[22px] text-white">
-                                        The Batman in Love
+                                <img
+                                    src="/images/featured-1.png"
+                                    className="object-cover rounded-[30px] w-[520px] h-[340px]"
+                                    alt=""
+                                />
+
+                                {/* rating */}
+                                <div className="rating absolute top-0 left-0">
+                                    <div className="p-[30px] flex items-center gap-1">
+                                        <img src="/icons/ic_star.svg" alt="" />
+                                        <span className="text-sm font-medium text-white mt-1">
+                                            4.5/5.0
+                                        </span>
                                     </div>
-                                    <p className="mb-0 text-white text-sm font-light">
-                                        Action • Horror
-                                    </p>
                                 </div>
-                                <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
+
+                                {/* bottom detail */}
+                                <div
+                                    className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px]
+                          rounded-br-[28px] flex justify-between items-center px-7 h-[130px]"
+                                >
+                                    <div>
+                                        <div className="font-medium text-[22px] text-white">
+                                            The Batman in Love
+                                        </div>
+                                        <p className="mb-0 text-white text-sm font-light">
+                                            Action • Horror
+                                        </p>
+                                    </div>
+                                    <div className="translate-x-[100px] group-hover:translate-x-0 transition ease-in-out duration-500">
+                                        <img
+                                            src="/icons/ic_play.svg"
+                                            width={50}
+                                            alt=""
+                                        />
+                                    </div>
+                                </div>
+                                <a
+                                    href="watching.html"
+                                    className="inset-0 absolute z-50"
+                                />
+                            </div>
+                        ))}
+                    </Flickity>
+                </div>
+                {/* /Featured */}
+
+                {/* Browse */}
+                <div className="mt-14">
+                    <div className="font-semibold text-[22px] text-black mb-4">
+                        Browse
+                    </div>
+                    <Flickity className="gap-[30px]" options={flickityOptions}>
+                        {[...Array(6)].map((i) => (
+                            <div className="absolute group overflow-hidden mr-[30px]">
+                                <img
+                                    src="/images/browse-1.png"
+                                    className="object-cover rounded-[30px] h-[340px] w-[250px]"
+                                    alt=""
+                                />
+                                <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
+                                    <div className="px-7 pb-7">
+                                        <div className="font-medium text-xl text-white">
+                                            Meong Golden
+                                        </div>
+                                        <p className="mb-0 text-gray-300 text-base mt-[10px]">
+                                            Horror • Love
+                                        </p>
+                                    </div>
+                                </div>
+                                <div
+                                    className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
+                          -translate-x-1/2 z-20 transition ease-in-out duration-500"
+                                >
                                     <img
                                         src="/icons/ic_play.svg"
+                                        className=""
                                         width={50}
                                         alt=""
                                     />
                                 </div>
+                                <a
+                                    href="watching.html"
+                                    className="inset-0 absolute z-50"
+                                />
                             </div>
-                            <a
-                                href="watching.html"
-                                className="inset-0 absolute z-50"
-                            />
-                        </div>
-                    ))}
-                </Flickity>
-            </div>
-            {/* /Featured */}
-        </Authenticated>
+                        ))}
+                    </Flickity>
+                </div>
+                {/* /Continue Watching */}
+            </Authenticated>
+        </>
     );
 }
