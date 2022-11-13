b5fdab33c20329ec9390018b0af6fdb9efb1069b
Ikram Maulana <ikram075@ummi.ac.id>
2022-11-14 05:47:59 +0700

    Feat: Slicing Movie Card Component

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -5,6 +5,7 @@ import Flickity from "react-flickity-component";
 // CSS for Flickity
 import "flickity/css/flickity.css";
 import { Head } from "@inertiajs/inertia-react";
+import MovieCard from "@/Components/MovieCard";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -32,10 +33,7 @@ export default function Dashboard() {
 
                     <Flickity className="gap-[30px]" options={flickityOptions}>
                         {[...Array(4)].map((i) => (
-                            <div
-                                className="absolute overflow-hidden group mr-[30px]"
-                                key={i}
-                            >
+                            <div className="absolute overflow-hidden group mr-[30px]">
                                 <img
                                     src="/images/featured-1.png"
                                     className="object-cover rounded-[30px] w-[520px] h-[340px]"
@@ -90,38 +88,13 @@ export default function Dashboard() {
                     </div>
                     <Flickity className="gap-[30px]" options={flickityOptions}>
                         {[...Array(6)].map((i) => (
-                            <div className="absolute group overflow-hidden mr-[30px]">
-                                <img
-                                    src="/images/browse-1.png"
-                                    className="object-cover rounded-[30px] h-[340px] w-[250px]"
-                                    alt=""
-                                />
-                                <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
-                                    <div className="px-7 pb-7">
-                                        <div className="font-medium text-xl text-white">
-                                            Meong Golden
-                                        </div>
-                                        <p className="mb-0 text-gray-300 text-base mt-[10px]">
-                                            Horror • Love
-                                        </p>
-                                    </div>
-                                </div>
-                                <div
-                                    className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
-                          -translate-x-1/2 z-20 transition ease-in-out duration-500"
-                                >
-                                    <img
-                                        src="/icons/ic_play.svg"
-                                        className=""
-                                        width={50}
-                                        alt=""
-                                    />
-                                </div>
-                                <a
-                                    href="watching.html"
-                                    className="inset-0 absolute z-50"
-                                />
-                            </div>
+                            <MovieCard
+                                key={i}
+                                slug="the-batman-in-love"
+                                name={`The Batman in Love ${i}`}
+                                thumbnail="https://picsum.photos/seed/picsum/300/300"
+                                category="Comedy"
+                            />
                         ))}
                     </Flickity>
                 </div>
