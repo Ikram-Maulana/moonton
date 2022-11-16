0c79f0215f57e573816efb20ff38c99e5dcccd33
Ikram Maulana <ikram075@ummi.ac.id>
2022-11-16 17:47:34 +0700

    Feat: Admin Init Create Movie

---
+++ resources/js/Pages/Admin/Movie/Create.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Create() {
+    return <h1>Create Movie Page</h1>;
+}
--- resources/js/Pages/Admin/Movie/Index.jsx
+++ resources/js/Pages/Admin/Movie/Index.jsx
@@ -1,5 +1,16 @@
+import PrimaryButton from "@/Components/PrimaryButton";
+import Authenticated from "@/Layouts/Authenticated/Index";
+import { Link } from "@inertiajs/inertia-react";
 import React from "react";
 
-export default function Index() {
-    return <div>Hello World</div>;
+export default function Index({ auth }) {
+    return (
+        <Authenticated auth={auth}>
+            <Link href={route("admin.dashboard.movie.create")}>
+                <PrimaryButton type="button" className="w-40 mb-8">
+                    Insert New Movie
+                </PrimaryButton>
+            </Link>
+        </Authenticated>
+    );
 }
